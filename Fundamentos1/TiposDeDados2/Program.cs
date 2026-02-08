90e28476ce17afbce8487ef923ef59caff032709
Alex Pietro <alexpieetro@gmail.com>
2026-02-08 16:45:14 -0300

    Fix: limpando cache para aplicar .gitignore

---
+++ Fundamentos1/TiposDeDados2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Atribuindo Valores a Double, Float e Decimal:");
+Console.WriteLine("");
+
+//Double n Precisa de sufixo, pois o valor é do tipo double por padrão
+double n1 = 1.234;
+
+//Float n Precisa de sufixo "f" para indicar que é do tipo float
+float n2 = 1.234f;
+
+//Decimal Precisa de sufixo "m" para indicar que é do tipo decimal
+decimal n3;
+n3 = 1.234m;
+
+//Aqui Serve para Comparar a Precisão de cada Tipo Decimal, pois o valor é o mesmo, mas a precisão é diferente
+float x = 1.0f / 3.0f;
+double y = 1.0 / 3.0;
+decimal z = 1.0m / 3.0m;
+
+
+Console.WriteLine("------------------------------");
+Console.WriteLine("Valores Sendo definido na variavel:");
+Console.WriteLine("");
+Console.WriteLine("Valor do Double: " + n1);
+Console.WriteLine("Valor do Float: " + n2);
+Console.WriteLine("------------------------------");
+Console.WriteLine("Valor sendo definido Após:");
+Console.WriteLine("");
+Console.WriteLine("Valor do Decimal: " + n3);
+Console.WriteLine("------------------------------");
+
+Console.ReadLine();
+
+Console.WriteLine("Comparando a precisão de Float, Double e Decimal");
+
+
+Console.WriteLine("------------------------------");
+Console.WriteLine("Precisão de cada Tipo Decimal:");
+Console.WriteLine("");
+Console.WriteLine("Valor de Precisão do Float 15 a 17 digitos: " + x);
+Console.WriteLine("Valor de Precisão do Double 7 digitos: " + y);
+Console.WriteLine("Valor de Precisão do Decimal 28 a 29 digitos: " + z);
+Console.WriteLine("------------------------------");
+
+Console.ReadLine();
